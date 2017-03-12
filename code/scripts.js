3978dc142791720c16c31ad7b927f16d5bcd4e18
Aellon Krider <aellonkrider@gmail.com>
2017-03-11 22:59:54 -0500

    update code

--- code/scripts.js
+++ code/scripts.js
@@ -1,35 +1,14 @@
 // slick slider initializer //
 
 $(document).ready(function(){
-  $('.slideshow').slick({
+  $('.slider').slick({
     slidesToShow: 2,
     slidesToScroll: 2,
     arrows: true,    
     dots: false,
-    speed: 100,
-    fade: true,
-    infinite: true
+    infinite: true,
+    speed: 5,
+    cssEase: 'ease'
   });
 });
 
-var prevSlideInterval = null,
-    nextSlideInterval = null;
-
-function prevSlideAnimation() { $('.slideshow').slickPrev(); };
-function nextSlideAnimation() { $('.slideshow').slickNext(); };
-
-$('.slick-prev').on('mouseenter', function() {
-  prevSlideInterval = window.setInterval(function(){ prevSlideAnimation() }, 200);
-});
-
-$('.slick-prev').on('mouseleave', function() {
-  window.clearInterval(prevSlideInterval);
-});
-
-$('.slick-next').on('mouseenter', function(){
-  nextSlideInterval = window.setInterval(function(){ nextSlideAnimation() }, 200);
-});
-
-$('.slick-next').on('mouseleave', function() {
-  window.clearInterval(nextSlideInterval);
-});
