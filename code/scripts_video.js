fbd73ac00b1341f602ee323ec1262903c0d8f450
Aellon Krider <aellonkrider@gmail.com>
2017-05-06 18:19:27 -0400

    update media queries

--- code/scripts_video.js
+++ code/scripts_video.js
@@ -11,19 +11,7 @@ $(document).ready(function(){
     speed: 5,
     cssEase: 'ease',
     appendArrows: $('.button'),
-    responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 320,
-      settings: "unslick"
-  	}
-  ]
+    
   });
 });
 
